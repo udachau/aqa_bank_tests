4638f10672a15181a4efab9a3fe3ec448d0a46ee
udachau <escape.pro@yandex.ru>
2026-09-02 09:32:44 +0300

    Другие пользаки в тесте

--- src/main/api/tests/transfer_account_test.py
+++ src/main/api/tests/transfer_account_test.py
@@ -12,8 +12,8 @@ from src.main.api.models.deposit_account_request import DepositAccountRequest
 @pytest.mark.api
 class TestAccountTransfer:
     def test_account_transfer(self):
-        create_user1_request = CreateUserRequest(username="MaxFrom1", password="Pas!sw0rd", role="ROLE_USER")
-        create_user2_request = CreateUserRequest(username="MaxTo1", password="Pas!sw0rd", role="ROLE_USER")
+        create_user1_request = CreateUserRequest(username="MaxFrom2", password="Pas!sw0rd", role="ROLE_USER")
+        create_user2_request = CreateUserRequest(username="MaxTo2", password="Pas!sw0rd", role="ROLE_USER")
 
         CreateUserRequester(
             request_spec=RequestSpecs.auth_headers(username="admin", password="123456"),
@@ -26,7 +26,7 @@ class TestAccountTransfer:
         ).post(create_user2_request)
 
         resp_creation_balance = CreateAccountRequester(
-            request_spec=RequestSpecs.auth_headers(username="MaxFrom1", password="Pas!sw0rd"),
+            request_spec=RequestSpecs.auth_headers(username="MaxFrom2", password="Pas!sw0rd"),
             response_spec=ResponseSpecs.request_created()
         ).post()
 
@@ -35,12 +35,12 @@ class TestAccountTransfer:
         deposit_account_request_from = DepositAccountRequest(accountId=balance_id_from, amount=2001)
 
         DepositAccountRequester(
-            request_spec=RequestSpecs.auth_headers(username="MaxFrom1", password="Pas!sw0rd"),
+            request_spec=RequestSpecs.auth_headers(username="MaxFrom2", password="Pas!sw0rd"),
             response_spec=ResponseSpecs.request_ok()
         ).post(deposit_account_request_from)
 
         resp_creation_balance = CreateAccountRequester(
-            request_spec=RequestSpecs.auth_headers(username="MaxTo1", password="Pas!sw0rd"),
+            request_spec=RequestSpecs.auth_headers(username="MaxTo2", password="Pas!sw0rd"),
             response_spec=ResponseSpecs.request_created()
         ).post()
 
@@ -49,7 +49,7 @@ class TestAccountTransfer:
         transfer_account_request = TransferAccountRequest(fromAccountId=balance_id_from, toAccountId=balance_id_to, amount=1000)
 
         response = TransferAccountRequester(
-            request_spec=RequestSpecs.auth_headers(username="MaxFrom1", password="Pas!sw0rd"),
+            request_spec=RequestSpecs.auth_headers(username="MaxFrom2", password="Pas!sw0rd"),
             response_spec = ResponseSpecs.request_ok()
         ).post(transfer_account_request)
 
